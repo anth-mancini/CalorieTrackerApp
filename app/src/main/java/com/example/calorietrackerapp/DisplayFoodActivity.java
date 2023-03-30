b3f5dba1f4f30c742cb8125864f996af137a9873
mancinii5 <mancini5@uwindsor.ca>
2023-03-30 17:28:03 -0400

    Clear list function implemented

--- app/src/main/java/com/example/calorietrackerapp/DisplayFoodActivity.java
+++ app/src/main/java/com/example/calorietrackerapp/DisplayFoodActivity.java
@@ -2,7 +2,9 @@ package com.example.calorietrackerapp;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,7 @@ public class DisplayFoodActivity extends AppCompatActivity {
 
     private ArrayList<Food> foodList;
     private SharedPreferences prefs;
+    Button btnClear;
     private Gson gson;
 
     private ListView foodListView;
@@ -27,6 +30,7 @@ public class DisplayFoodActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_food);
 
         foodListView = findViewById(R.id.foodListView);
+        btnClear = findViewById(R.id.btnClear);
 
         prefs = getSharedPreferences("userPrefs", MODE_PRIVATE);
         gson = new Gson();
@@ -41,6 +45,28 @@ public class DisplayFoodActivity extends AppCompatActivity {
 
         ArrayAdapter<Food> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foodList);
         foodListView.setAdapter(adapter);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteAllFoods();
+            }
+        });
     }
+
+    private void deleteAllFoods() {
+        foodList.clear();
+
+        // Save the updated food list to SharedPreferences
+        SharedPreferences.Editor editor = prefs.edit();
+        String json = gson.toJson(foodList);
+        editor.putString("foodList", json);
+        editor.apply();
+
+        // Update the ListView to reflect the changes
+        ArrayAdapter<Food> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foodList);
+        foodListView.setAdapter(adapter);
+    }
+
 }
 
